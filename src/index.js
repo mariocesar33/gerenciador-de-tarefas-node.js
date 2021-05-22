e59f3a0c4ac1fff54520afcaf55919084f9614a6
Mário César Silva <mcgato33@hotmail.com>
2021-05-22 22:24:33 -0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
+
+{/**
+  id: 'uuid', // precisa ser um uuid
+  name: 'Danilo Vieira',
+  username: 'danilo', 
+  todos: []
+*/}
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { id } = request.headers;
+
+  const user = users.find( user => user. id === id);
+
+  if(!user) {
+    return response.status(400).json({ error: "usuário não exist"})
+  }
+
+  request.user = user; 
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
   // Complete aqui
+  const { name, userName } = request.body;
+
+  users.push({
+    id: uuidv4,
+    name,
+    userName,
+    todos: [],
+  });
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
